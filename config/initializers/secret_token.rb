2c8c89df42a5051ed3dfd59843081a86a34beda1
Merouane Atig <merouane.atig@gmail.com>
2014-01-13 16:01:49 +0000

    create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mealz::Application.config.secret_key_base = 'a8c332c730db0c116fe63e158c760eabbdf9d40d16d4e6c050a81878388823dc9653b619607876c6f5329d1fecdef47645fa6f197cbab76b6d6f41ad1363ee76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mealz::Application.config.session_store :cookie_store, key: '_mealz_session'
