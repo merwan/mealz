fc0c83a503fb1e9ab786f5fe4e96208780953cb9
Merouane Atig <merouane.atig@gmail.com>
2014-01-25 14:06:08 +0000

    create Tag model and associate with recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
+  has_many :taggings
+  has_many :tags, through: :taggings
   validates :title, :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114224211) do
+ActiveRecord::Schema.define(version: 20140125135917) do
 
   create_table "ingredients", force: true do |t|
     t.integer  "recipe_id"
@@ -31,6 +31,22 @@ ActiveRecord::Schema.define(version: 20140114224211) do
   create_table "recipes", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["recipe_id"], name: "index_taggings_on_recipe_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
