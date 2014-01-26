45a038218ef1fdb0f97df62cb3ffb5f6a47225db
Merouane Atig <merouane.atig@gmail.com>
2014-01-26 22:47:42 +0100

    validation for category name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
 describe Category do
+  let (:category) { Category.new(name: 'fruits') }
+
   it 'has many products' do
-    category = Category.new
     expect(category).to respond_to(:products)
   end
 
   it 'displays name for #to_s' do
-    category = Category.new(name: 'fruits')
     expect(category.to_s).to eq('fruits')
+  end
+
+  it 'is valid with a name' do
+    expect(category).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    category.name = nil
+    expect(category).not_to be_valid
   end
 end
