4af502ff26c11346e50638e4bc24079963456247
Merouane Atig <merouane.atig@gmail.com>
2014-01-26 16:30:12 +0100

    display category name for to_s

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,4 +5,9 @@ describe Category do
     category = Category.new
     expect(category).to respond_to(:products)
   end
+
+  it 'displays name for #to_s' do
+    category = Category.new(name: 'fruits')
+    expect(category.to_s).to eq('fruits')
+  end
 end
