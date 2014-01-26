32798a1df861069ee3e7229d3ca3bbe4c3078375
Merouane Atig <merouane.atig@gmail.com>
2014-01-26 16:37:04 +0100

    assign a category to a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :load_categories, only: [:new, :edit, :create]
 
   # GET /products
   # GET /products.json
@@ -62,13 +63,17 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:title)
-    end
+  def load_categories
+    @categories = Category.all.collect { |c| [c.name, c.id] }
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:title, :category_id)
+  end
 end
