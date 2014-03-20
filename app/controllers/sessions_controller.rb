7873e7443d781fe66a3f8180134e9a4e918fd114
Merouane Atig <merouane.atig@gmail.com>
2014-03-20 16:23:29 +0000

    add user model and create/find user from omniauth callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,12 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
 
   def create
-    render text: request.env['omniauth.auth']
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127200415) do
+ActiveRecord::Schema.define(version: 20140320161144) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -80,6 +80,14 @@ ActiveRecord::Schema.define(version: 20140127200415) do
 
   create_table "units", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
