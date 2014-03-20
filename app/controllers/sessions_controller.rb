6377e103a77497c6905a0734169f8ad5598140fd
Merouane Atig <merouane.atig@gmail.com>
2014-03-20 21:30:05 +0100

    redirect to root after authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
+    redirect_to root_path, notice: "Logged in as #{@user.name}"
   end
 
   private
